646acc142664f3cc79e87923aeeeb46744ef0cc5
Zander Wepener <ZanderWepener5@gmail.com>
2025-12-27 15:53:05 +0200

    Create PROBLEM_SET_2_HAPPY_TO_CONNECT.sql

---
+++ PROBLEM_SET_2_HAPPY_TO_CONNECT.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "Users"(
+    "Id" INTEGER PRIMARY KEY,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "Username" TEXT,
+    "Password" TEXT
+
+);
+
+CREATE TABLE "Schools_And_Universities"(
+    "School_Name" TEXT,
+    "School_Type" TEXT,
+    "School_Location" TEXT,
+    "Year_Founded_In" TEXT
+
+);
+
+CREATE TABLE "Companies"(
+    "Company_Name" TEXT,
+    "Industry" TEXT,
+    "Location" TEXT
+
+);
